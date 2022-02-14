d86ddfa848bd29cb70ba796339a886f016508988
Bisma Baig <baigbisma1@gmail.com>
2022-02-13 20:24:25 -0800

    Used Express framework to add routes.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-var http = require('http');
 
-//create a server object
-http.createServer(function (req, res) {
-  res.write('Hello World'); // write response to client
-  res.end(); //end response
-}).listen(8080);// server listens on port 8080
+const express = require('express'); 
+const app = express();              
+const port = 5000; 
+
+app.get('/', (req, res) => {
+  res.type('text/html');
+    res.sendFile('index.html', {root: __dirname});
+});
+
+app.get('/about', (req,res) => {
+  res.type('text/html');
+  res.sendFile('about.html', {root: __dirname});
+});
+
+app.use((req,res) => {
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not found');
+ });
+
+app.listen(port, () => { 
+  console.log('Express started');
+});
